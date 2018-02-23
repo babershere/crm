96f62dbfc7b87a7f5887ef16aa2101d670ea39c9
babershere <rehmatq@gmail.com>
2018-02-22 19:19:14 -0800

    crm routes

--- app_client/src/App.js
+++ app_client/src/App.js
@@ -1,30 +1,33 @@
 import React, { Component } from 'react';
-import {browserRouter as Router, route} from "react-router-dom";
-import Home from ".components/Home";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+// employee components
+import EmployeeHome from "./components/employees/EmployeeHome";
+import EmployeeAdd from "./components/employees/EmployeeAdd";
+import EmployeeEdit from "./components/employees/EmployeeEdit";
+import EmployeeView from "./components/employees/EmployeeView";
 
-//employee
-import employee from "./components/employees/employee"
-import employee from "./components/employees/employee"
-import employee from "./components/employees/employee"
-import employee from "./components/employees/employee"
 
-
+import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="">
-      <Router>
-        <div>
-          <NavBar/>
-      <Router exact path = "/" component={home}/>
-          <Router exact path="/employees" component={Employeehome} />
-          <Router exact path="/employees/add" component={EmployeeAdd}/>
-          <Router exact path="/employees/edit/:id" component={EmployeeEdit}/>
-          <Router exact path="/employees/view/:id" component={EmployeeView}/>
+      <div>
+        <Router>
+          <div>
+            <Navbar />
+            <Route exact path="/" component={Home} />
+            <Route exact path="/employees" component={EmployeeHome} />
+            <Route exact path="/employees/add" component={EmployeeAdd} />
+            <Route exact path="/employees/edit/:id" component={EmployeeEdit} />
+            <Route exact path="/employees/view/:id" component={EmployeeView} />
+            {/* footer go here yo */}
+          </div>
+        </Router>
       </div>
-      </Router>
     );
   }
 }
